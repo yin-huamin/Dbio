da6e19386ef9b09179e65173ce2a3b51314f8a41
yin-huamin <yinhm17@126.com>
2022-03-30 22:19:52 +0800

    Preproces clinical data from GEO

---
+++ R/preprocess_sample.R
@@ -0,0 +1,23 @@
+#' Preproces clinical data from GEO
+#'
+#' @param metadata Sample metadata get from GEO
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' library(Dbio)
+#' library(GEOquery)
+#' sample <- get_sample("GSE54566", "./")
+#' sample_post <- preprocess_sample(sample)
+preprocess_sample <- function(metadata = sample) {
+  dat <- metadata[, c(1, grep(":ch1$", colnames(metadata)))]
+
+  colnames(dat) <- gsub(":ch1$", "", colnames(dat))
+
+  res <- dat %>%
+    rownames_to_column("GSM") %>%
+    dplyr::distinct(GSM, .keep_all = TRUE)
+
+  return(res)
+}
