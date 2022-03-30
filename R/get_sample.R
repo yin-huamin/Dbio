a814edd1847e54aec3644c9def90af526029ab23
yin-huamin <yinhm17@126.com>
2022-03-30 13:14:16 +0800

    Initial commit

---
+++ R/get_sample.R
@@ -0,0 +1,29 @@
+#' Get clinical data from GEO
+#'
+#' @param GEO_name GEO number
+#' @param dir Dest dir
+#'
+#' @return clinical data of GSE you choosed
+#' @export
+#'
+#' @examples
+#' #' library(Dbioinfo)
+#' get_sample("GSE555", "./")
+get_sample <- function(GEO_name = GEO_name, dir = dir) {
+  if (requireNamespace("GEOquery", quietly = TRUE)) {
+    des_dir <- paste0(dir, GEO_name)
+    gset <- GEOquery::getGEO(
+      GEO = GEO_name,
+      destdir = paste0(des_dir, "/1.rawdata"),
+      AnnotGPL = F,
+      getGPL = F
+    )
+    sample <- Biobase::pData(gset[[1]])
+  } else {
+    stop(
+      "Package \"GEOquery\" must be installed to use this function.",
+      call. = FALSE
+    )
+  }
+  return(sample)
+}
---
+++ R/initial_file.R
@@ -0,0 +1,23 @@
+#' Initial file structure
+#'
+#' @param GEO_name GSE number
+#' @param dir Dest dir
+#'
+#' @return GSE download File structer
+#' @export
+#'
+#' @examples
+#' library(Dbioinfo)
+#' initial_file("GSE555", "./")
+initial_file <- function(GEO_name = GEO_name, dir = dir) {
+  des_dir <- paste0(dir, GEO_name)
+  if (!dir.exists(des_dir)) {
+    dir.create(des_dir)
+  }
+  if (!dir.exists(paste0(des_dir, "/1.rawdata"))) {
+    dir.create(paste0(des_dir, "/1.rawdata"))
+  }
+  if (!dir.exists(paste0(des_dir, "/2.output"))) {
+    dir.create(paste0(des_dir, "/2.output"))
+  }
+}
