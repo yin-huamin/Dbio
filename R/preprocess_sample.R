3ac273f881d4023690fae061bba0fc8cfde66f6e
yin-huamin <yinhm17@126.com>
2022-04-17 19:41:07 +0800

    Download clinical data

--- R/preprocess_sample.R
+++ R/preprocess_sample.R
@@ -11,9 +11,9 @@
 #' sample <- get_sample("GSE54566", "./")
 #' sample_post <- preprocess_sample(sample)
 preprocess_sample <- function(metadata = sample) {
-  dat <- metadata[, c(1, grep(":ch1$", colnames(metadata)))]
+  dat <- metadata[, c(1, grep("_ch1$", colnames(metadata)))]
 
-  colnames(dat) <- gsub(":ch1$", "", colnames(dat))
+  colnames(dat) <- gsub("ch1$", "", colnames(dat))
 
   res <- dat %>%
     rownames_to_column("GSM") %>%
